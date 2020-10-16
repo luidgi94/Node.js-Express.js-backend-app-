7e186d9875188a8a6f6ea1fb82c3a1d1311468f5
luidgi clairboy <luidgi-clairboy@live.fr>
2020-10-16 15:36:31 +0200

    ajout models

--- app.js
+++ app.js
@@ -1,3 +1,52 @@
 const express = require('express');
 const app = express();
+const Thing = require('./models/thing');
+/* connection au cluster mongoDB */
+const mongoose = require('mongoose');
+mongoose.connect('mongodb+srv://luidgi:underworld@clusterbyluidgi.bw0ue.mongodb.net/Clusterbyluidgi?retryWrites=true&w=majority', 
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+
+const bodyParser = require('body-parser'); // dependance qui traduit le json recu par la requette en objet javascript utilisable
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+  });
+
+app.use(bodyParser.json());
+app.post('/api/stuff', (req, res, next) => { // gère que les requetes post: creation d'enregistrement d'objet dans la base de données
+  console.log(req.body);
+  delete req.body._id;
+  const thing = new Thing({
+    ...req.body
+  });
+  thing.save()
+    .then(() => res.status(201).json({ message: 'Objet enregistré !'}))
+    .catch(error => res.status(400).json({ error }));
+});
+
+app.get('/api/stuff/:id', (req, res, next) => {
+  Thing.findOne({ _id: req.params.id })
+    .then(thing => res.status(200).json(thing))
+    .catch(error => res.status(404).json({ error }));
+});
+
+app.put('/api/stuff/:id', (req, res, next) => {
+  Thing.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
+    .then(() => res.status(200).json({ message: 'Objet modifié !'}))
+    .catch(error => res.status(400).json({ error }));
+});
+
+
+app.use('/api/stuff', (req, res, next) => { // attention ! gère tous types de requetes mettre les requetes post avant! 
+    Thing.find() // renvoi tous les objets de la collection Thing sur mongoDB
+    .then(things => res.status(200).json(things))
+    .catch(error => res.status(400).json({ error }));
+  });
+ 
 module.exports = app; // on export l'app express pour l'uttiliser dans toute l'application
--- server.js
+++ server.js
@@ -1,11 +1,48 @@
+console.log('on voit le flux du serveur en faisant une cmd : node server ou nodemon server ce qui est mieux');
+
 // Ici, vous importez le package HTTP natif de Node et l'utilisez pour créer un serveur, en passant une fonction qui sera exécutée à chaque appel effectué vers ce serveur. Cette fonction reçoit les objets request et response en tant qu'arguments. Dans cet exemple,
 //  vous utilisez la méthode end de la réponse pour renvoyer une réponse de type string à l'appelant.
 const http = require('http'); 
-console.log('on voit le flux du serveur en faisant une cmd : node server 2');
-const server= http.createServer((req,res)=>{ // on crée serveur node  avec 2 arguments res et req
-    res.end('Voila la reponse du serveur! ');
-});
-server.listen(process.env.PORT || 3000);
+const app = require('./app'); // on importe express
+const normalizePort = val => { // config du port d'écoute des requettes . renvoie un port valide, qu'il soit fourni sous la forme d'un numéro ou d'une chaîne ;
+    const port = parseInt(val, 10);
+  
+    if (isNaN(port)) {
+      return val;
+    }
+    if (port >= 0) {
+      return port;
+    }
+    return false;
+  };
+  const port = normalizePort(process.env.PORT || '3000');
+  app.set('port', port);
+  
+  const errorHandler = error => { // recherche les différentes erreurs et les gère de manière appropriée. Elle est ensuite enregistrée dans le serveur ;
+    if (error.syscall !== 'listen') {
+      throw error;
+    }
+    const address = server.address();
+    const bind = typeof address === 'string' ? 'pipe ' + address : 'port: ' + port;
+    switch (error.code) {
+      case 'EACCES':
+        console.error(bind + ' requires elevated privileges.');
+        process.exit(1);
+        break;
+      case 'EADDRINUSE':
+        console.error(bind + ' is already in use.');
+        process.exit(1);
+        break;
+      default:
+        throw error;
+    }
+  };
+  
+
+const server= http.createServer(app) ;// on crée serveur node  avec l'argument app de express
+
+
+
 // Dans la dernière ligne, vous configurez le serveur pour qu'il écoute :
 // soit la variable d'environnement du port grâce à process.env.PORT : si la plateforme de déploiement propose un port par défaut, c'est celui-ci qu'on écoutera ;
 // soit le port 3000, ce qui nous servira dans le cas de notre plateforme de développement.
@@ -13,3 +50,16 @@ server.listen(process.env.PORT || 3000);
 // Démarrez le serveur en exécutant node server à partir de la ligne de commande. Pour vérifier qu'il envoie la réponse correcte,
 //  utilisez une fenêtre de navigateur pour accéder à http://localhost:3000 
 
+server.on('error', errorHandler);
+server.on('listening', () => {
+  const address = server.address();
+  const bind = typeof address === 'string' ? 'pipe ' + address : 'port ' + port;
+  console.log('Listening on ' + bind);
+});
+
+server.listen(port); // un écouteur d'évènements est également enregistré, consignant le port ou le canal nommé sur lequel le serveur s'exécute dans la console.
+
+
+//CORS signifie « Cross Origin Resource Sharing ». Il s'agit d'un système de sécurité qui, par défaut,
+// bloque les appels HTTP d'être effectués entre des serveurs différents, ce qui empêche donc les requêtes malveillantes d'accéder à des ressources sensibles. Dans notre cas, nous avons deux origines : localhost:3000 et localhost:4200 , et nous souhaiterions qu'elles puissent communiquer entre elles.
+// Pour cela, nous devons ajouter des headers à notre objet  response .
